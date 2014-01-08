ef314d7f347490dde1a42c1850b2679ae8083674
Yash Varma <vyash8@gmail.com>
2014-01-08 15:09:19 -0600

    first commit

---
+++ Adventure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adventure
+//
+//  Created by Yash Varma on 1/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Adventure/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Adventure
+//
+//  Created by Yash Varma on 1/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
